a5fce90108963f9d32f790e6d783b57084e1a8e7
Ali Amirnezhad <webilix@gmail.com>
2025-01-02 01:40:15 +0330

    Bugfix: form error directive

--- ngx-form-m3/src/lib/directives/form-error.directive.ts
+++ ngx-form-m3/src/lib/directives/form-error.directive.ts
@@ -4,20 +4,21 @@ import { FormGroupDirective } from '@angular/forms';
 @Directive({ selector: 'form.ngx-form' })
 export class FormErrorDirective {
     @HostListener('submit') onSubmit(): void {
-        if (this.formGroupDirective && this.formGroupDirective.control.invalid) {
-            const invalidControl: HTMLElement = this.elementRef.nativeElement.querySelector('.ng-invalid');
+        setTimeout(() => {
+            const invalidControl: HTMLElement = this.elementRef.nativeElement.querySelector('.mat-form-field-invalid');
             if (invalidControl) return this.scrollToElement(invalidControl);
-        }
 
-        const invalidControl: HTMLElement = this.elementRef.nativeElement.querySelector('.ngx-form-invalid');
-        if (invalidControl) this.scrollToElement(invalidControl);
+            console.log(invalidControl);
+        }, 150);
     }
 
     constructor(private elementRef: ElementRef, @Optional() private formGroupDirective: FormGroupDirective) {}
 
+    private getElementTop(element: HTMLElement): number {
+        return element.getBoundingClientRect().top + window.scrollY - 100;
+    }
+
     private scrollToElement(element: HTMLElement): void {
-        const labelOffset: number = 150;
-        const top: number = element.getBoundingClientRect().top + window.scrollY - labelOffset;
-        window.scroll({ top, left: 0, behavior: 'smooth' });
+        window.scroll({ top: this.getElementTop(element), behavior: 'smooth' });
     }
 }
